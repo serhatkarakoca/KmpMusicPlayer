0f98b12bb3be4b30fb9077a1713435486184112a
serhat <serhatkarakoca@gmail.com>
2025-08-12 08:30:34 +0300

    enabled edgeToEdge

--- composeApp/src/androidMain/kotlin/com/karakoca/baseproject/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/karakoca/baseproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.karakoca.baseproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.karakoca.baseproject.presentation.App
@@ -12,6 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            enableEdgeToEdge()
             App()
         }
     }
--- composeApp/src/commonMain/kotlin/com/karakoca/baseproject/presentation/App.kt
+++ composeApp/src/commonMain/kotlin/com/karakoca/baseproject/presentation/App.kt
@@ -1,17 +1,9 @@
 package com.karakoca.baseproject.presentation
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.karakoca.baseproject.di.getSharedModules
 import com.karakoca.baseproject.ui.MyApplicationTheme
@@ -29,9 +21,7 @@ fun App() {
         MyApplicationTheme {
             Surface(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .systemBarsPadding()
-                    .imePadding(),
+                    .fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
                 MainNavController()
